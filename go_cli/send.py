c5467b2ad4fbb0dbc37809df077e5c69915489c9
Simon Cross <hodgestar@gmail.com>
2015-09-14 13:31:55 +0200

    Add CSV and JSON parsing.

--- go_cli/send.py
+++ go_cli/send.py
@@ -1,5 +1,8 @@
 """ Send messages via an HTTP API (nostream) conversation. """
 
+import csv
+import json
+
 import click
 
 from go_http.send import HttpApiSender
@@ -23,7 +26,33 @@ from go_http.send import HttpApiSender
 def send(ctx, conversation, token, csv, json):
     """ Send messages via an HTTP API (nostream) conversation.
     """
+    if not any((csv, json)):
+        click.echo("Please specify either --csv or --json.")
+        ctx.abort()
     http_api = HttpApiSender(ctx.obj.account_key, conversation, token)
-    messages = []  # TODO: parse csv or json
-    for msg in messages:
-        http_api.send_text(**msg)
+    if csv:
+        for msg in messages_from_csv(csv):
+            http_api.send_text(**msg)
+    if json:
+        for msg in messages_from_json(json):
+            http_api.send_text(**msg)
+
+
+def messages_from_csv(csv_file):
+    reader = csv.DictReader(csv_file)
+    for data in reader:
+        yield {
+            "to_addr": data["to_addr"],
+            "content": data["content"],
+            "session_event": data.get("session_event")
+        }
+
+
+def messages_from_json(json_file):
+    for line in json_file:
+        data = json.loads(line.rstrip("\n"))
+        yield {
+            "to_addr": data["to_addr"],
+            "content": data["content"],
+            "session_event": data.get("session_event")
+        }
