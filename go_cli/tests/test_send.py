92db578bcc23ecdd08e12e9dd9b6cc3fa204b550
Simon Cross <hodgestar@gmail.com>
2015-09-20 00:44:20 +0200

    Test sending messages.

--- go_cli/tests/test_send.py
+++ go_cli/tests/test_send.py
@@ -7,18 +7,73 @@ import types
 
 from click.testing import CliRunner
 
+import go_cli.send
 from go_cli.main import cli
 from go_cli.send import messages_from_csv, messages_from_json
+from go_cli.tests.utils import ApiHelper
 
 
 class TestSendCommand(TestCase):
+    def setUp(self):
+        self.runner = CliRunner()
+        self.api_helper = ApiHelper(self)
+        self.api_helper.patch_api(go_cli.send, 'HttpApiSender')
+
+    def tearDown(self):
+        self.api_helper.tearDown()
+
+    def invoke_send(self, args, account="acc-1", conv_key="conv-1",
+                    conv_token="tok-1"):
+        return self.runner.invoke(cli, [
+            '--account', account, 'send',
+            '--conversation', conv_key,
+            '--token', conv_token,
+        ] + args)
+
     def test_send_help(self):
-        runner = CliRunner()
-        result = runner.invoke(cli, ['send', '--help'])
+        result = self.runner.invoke(cli, ['send', '--help'])
         self.assertEqual(result.exit_code, 0)
         self.assertTrue(
             "Send messages via an HTTP API (nostream) conversation."
             in result.output)
+
+    def test_send_no_conversation_details(self):
+        result = self.runner.invoke(cli, ['send'])
+        self.assertEqual(result.exit_code, 1)
+        self.assertTrue(
+            "Please specify all of the account key, conversation key and"
+            " conversation authentication token. See --help."
+            in result.output)
+
+    def test_send_no_data(self):
+        result = self.invoke_send([])
+        self.assertEqual(result.exit_code, 1)
+        self.assertTrue("Please specify either --csv or --json."
+                        in result.output)
+
+    def test_send_csv(self):
+        response = self.api_helper.add_send("acc-1", "conv-1", "tok-1")
+        with self.runner.isolated_filesystem():
+            with open('msgs.csv', 'wb') as f:
+                f.write("to_addr,content\n")
+                f.write("+1234,hello\n")
+            result = self.invoke_send(['--csv', 'msgs.csv'])
+            self.assertEqual(result.output, "Sending message to +1234.\n")
+            self.api_helper.check_response(
+                response, 'PUT',
+                {u'content': u'hello', u'to_addr': u'+1234'})
+
+    def test_send_json(self):
+        response = self.api_helper.add_send("acc-1", "conv-1", "tok-1")
+        with self.runner.isolated_filesystem():
+            with open('msgs.json', 'wb') as f:
+                f.write(json.dumps({"to_addr": "+1234", "content": "hello"}))
+                f.write("\n")
+            result = self.invoke_send(['--json', 'msgs.json'])
+            self.assertEqual(result.output, "Sending message to +1234.\n")
+            self.api_helper.check_response(
+                response, 'PUT',
+                {u'content': u'hello', u'to_addr': u'+1234'})
 
 
 class TestMessagesFromCsv(TestCase):
