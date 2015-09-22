b5ae9476b5b4d3ba106bb3846bff26c2dd559300
Simon Cross <hodgestar@gmail.com>
2015-09-22 12:40:04 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="go-cli",
-    version="0.1.0",
+    version="0.1.1a0",
     url='http://github.com/praekelt/go-cli',
     license='BSD',
     description="A command-line interface for Vumi Go's HTTP APIs",
