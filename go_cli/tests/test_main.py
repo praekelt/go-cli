968ef4bfb57743328587f9f693a7c531e20cbce0
Simon Cross <hodgestar@gmail.com>
2015-10-15 17:50:34 +0200

    Check that export-contacts is included in the top-level command.

--- go_cli/tests/test_main.py
+++ go_cli/tests/test_main.py
@@ -13,8 +13,12 @@ class TestCli(TestCase):
         result = runner.invoke(cli, ['--help'])
         self.assertEqual(result.exit_code, 0)
         self.assertTrue("Vumi Go command line utility." in result.output)
-        self.assertTrue("send  Send messages via an HTTP API (nostream)..."
-                        in result.output)
+        self.assertTrue(
+            "export-contacts  Export contacts from the contacts API."
+            in result.output)
+        self.assertTrue(
+            "send             Send messages via an HTTP API (nostream)..."
+            in result.output)
 
     def test_version(self):
         runner = CliRunner()
